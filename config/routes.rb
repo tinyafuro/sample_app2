e1fb36849a9b71617c29d4bd11b01fb6e774483d
EC2 Default User <ec2-user@ip-172-31-11-173.us-east-2.compute.internal>
2018-07-15 15:37:28 +0000

    Finish user destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ SampleApp::Application.routes.draw do
     :registrations => "registrations"
   }
   root  'static_pages#home'
-  resources :users, only: [:show, :index]
+  resources :users, only: [:show, :index, :destroy]
   
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
